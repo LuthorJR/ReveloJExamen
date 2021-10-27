fd631e60d334736474af14fb933b5af3db2f533f
Jonathan Revelo <jonathan.revelo@icloud.com>
2021-10-26 23:14:44 -0500

    1

--- ReveloJExamen/App.xaml.cs
+++ ReveloJExamen/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ReveloJExamen
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Login();
         }
 
         protected override void OnStart()
