8cd649a5b1f482723bf18209f7a89da7f68d35c1
Jonathan Revelo <jonathan.revelo@icloud.com>
2021-12-02 22:17:28 -0500

    con

---
+++ ReveloJExamen/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ReveloJExamen.Models
+{
+    public class Estudiante
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { set; get; }
+        [MaxLength(50)]
+
+        public string Nombre { set; get; }
+        [MaxLength(25)]
+
+        public string Usuario { get; set; }
+        [MaxLength(25)]
+
+        public string Contrasenia { get; set; }
+        
+    }
+}
